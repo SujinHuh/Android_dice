ecb767883a04ce12bd99c0e58a5600f65a1113f1
SujinHuh <juin483@naver.com>
2022-05-23 14:42:09 +0900

    if else, random funtion

--- app/src/main/java/com/keti/dice_all/MainActivity.kt
+++ app/src/main/java/com/keti/dice_all/MainActivity.kt
@@ -18,11 +18,45 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val diceImage1 =  binding.dice1
+        val diceImage2 = binding.dice2
+
         binding.diceStartBtn.setOnClickListener {
 
             Toast.makeText(this, "주사위 Go", Toast.LENGTH_LONG).show()
             Log.d("MainActivity", Random.nextInt(1, 6).toString())
             Log.d("MainActivity", Random.nextInt(1, 6).toString())
+
+            val number1 = Random.nextInt(1, 6)
+            val number2 = Random.nextInt(1, 6)
+
+            if(number1 == 1){
+               diceImage1.setImageResource(R.drawable.dice_1)
+            } else if(number1 == 2){
+                diceImage1.setImageResource(R.drawable.dice_2)
+            } else if(number1 == 3){
+                diceImage1.setImageResource(R.drawable.dice_3)
+            } else if(number1 == 4){
+                diceImage1.setImageResource(R.drawable.dice_4)
+            } else if(number1 == 5){
+                diceImage1.setImageResource(R.drawable.dice_5)
+            } else if(number1 == 6){
+                diceImage1.setImageResource(R.drawable.dice_6)
+            }
+
+            if(number2 == 1){
+                diceImage2.setImageResource(R.drawable.dice_1)
+            } else if(number2 == 2){
+                diceImage2.setImageResource(R.drawable.dice_2)
+            } else if(number2 == 3){
+                diceImage1.setImageResource(R.drawable.dice_3)
+            } else if(number2 == 4){
+                diceImage1.setImageResource(R.drawable.dice_4)
+            } else if(number2 == 5){
+                diceImage1.setImageResource(R.drawable.dice_5)
+            } else if(number2 == 6){
+                diceImage1.setImageResource(R.drawable.dice_6)
+            }
 
 
         }
